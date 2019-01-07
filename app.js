31ecd60574740c00f3640eccac9d303b9cb30d1a
dheeraj <ahujadheeraj99.com>
2019-01-07 14:53:10 +0530

    movie app
    Signed-off-by: dheeraj <ahujadheeraj99.com>

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("homepage");
+})
+
+app.get("/movie",function(req,res){
+    var query = req.query.search;
+    //http://www.omdbapi.com/?s=frozen&apikey=thewdb
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url,function(error,response,body){
+        var Pdata = JSON.parse(body);
+        if(error){
+            res.send("404 notfound!!")
+        }
+        if(!error && response.statusCode == 200){
+            res.render("movies",{data:Pdata});
+        }
+    });
+});
+
+app.listen(2200,function(req,res){
+    console.log("Movie Search Started!!!");
+})
